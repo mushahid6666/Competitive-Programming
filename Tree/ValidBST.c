cfc905a94dbd1b4adea45727fdd5dff9e0273423
Mushahid Alam <mushahid@cs.wisc.edu>
2015-11-25 18:36:22 -0600

    email address commit change

--- Tree/ValidBST.c
+++ Tree/ValidBST.c
@@ -1,5 +1,5 @@
 /*Given a binary tree, determine if it is a valid binary search tree (BST).
-
+a
 Assume a BST is defined as follows:
 
 The left subtree of a node contains only nodes with keys less than the node’s key.
