43bdef6789a8e9b3e7605f6707396d8dd280b39e
Mushahid Alam <mushahid@cs.wisc.edu>
2015-12-05 16:38:46 -0600

    Total number of ways to reach nth step. allowed 1,2 steps at a time

---
+++ array/stairs.c
@@ -0,0 +1,38 @@
+/*You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example :
+
+Input : 3
+Return : 3
+
+Steps : [1 1 1], [1 2], [2 1]*/
+/**
+ * @input A : Integer
+ *
+ * @Output Integer
+ */
+ int fib(int n)
+{
+  /* Declare an array to store Fibonacci numbers. */
+  int f[n+1];
+  int i;
+
+  /* 0th and 1st number of the series are 0 and 1*/
+  f[0] = 0;
+  f[1] = 1;
+
+  for (i = 2; i <= n; i++)
+  {
+      /* Add the previous 2 numbers in the series
+         and store it */
+      f[i] = f[i-1] + f[i-2];
+  }
+
+  return f[n];
+}
+
+int climbStairs(int A) {
+    return fib(A+1);
+}
